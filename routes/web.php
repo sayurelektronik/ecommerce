3045366f007c658dc1ce8380a8daabf7360291cc
Mohamad Ardiansyah Rofii <sayurelektronik@gmail.com>
2024-01-20 20:04:40 +0700

    Admin Panel- Create separate Admin login page

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// route login
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
